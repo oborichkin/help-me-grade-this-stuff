f5e3415c6795fdde78baf673dd7ca5da24dad382
Pavel Oborin <oborin.p@gmail.com>
2019-05-12 17:53:37 +0300

    recreate_db.py now uses row limit

--- recreate_db.py
+++ recreate_db.py
@@ -11,18 +11,24 @@ if __name__ == '__main__':
     que = pd.read_csv("data/que.csv", sep='|')
     ans = pd.read_csv("data/ans.csv", sep='|')
 
+    row_limit = 10000
+
     for _, row in que.iterrows():
+        if row_limit <= 0:
+            exit
         q = Question(id=row['ID'],
                      text=row['QUESTIOIN'],
                      right_answer=row['TEACHER_ANSWER'])
         db.session.add(q)
+        row_limit -= 1
     db.session.commit()
 
     for _, row in ans.iterrows():
-        if row['ID'] == 9895:
+        if row_limit <= 0:
             exit
         a = Answer(id=row['ID'],
                    q_id=row['Q_ID'],
                    text=row['ANSWER'])
         db.session.add(a)
+        row_limit -= 1
     db.session.commit()

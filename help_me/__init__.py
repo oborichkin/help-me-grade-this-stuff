7a14503b0e40f9c589a345becc64a3cf516dfc7d
Pavel Oborin <oborin.p@gmail.com>
2019-05-12 17:18:08 +0300

    Hotfix

--- help_me/__init__.py
+++ help_me/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 heroku = Heroku(app)
+db = SQLAlchemy(app)
 
 from help_me import routes

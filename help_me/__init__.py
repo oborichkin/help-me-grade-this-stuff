9bbe1f66f3765851b664b446dc8ceba0ff9cf36d
Pavel Oborin <oborin.p@gmail.com>
2019-05-12 17:22:20 +0300

    Fixes

--- help_me/__init__.py
+++ help_me/__init__.py
@@ -6,7 +6,7 @@ from flask_heroku import Heroku
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 heroku = Heroku(app)

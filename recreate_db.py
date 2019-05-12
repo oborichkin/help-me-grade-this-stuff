4b82f26ab97657741eeb78e98b10bd83629efd5a
Pavel Oborin <oborin.p@gmail.com>
2019-05-12 16:49:51 +0300

    :card_file_box: Added data and script for db recreation

---
+++ recreate_db.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from help_me import app, db
+from help_me.models import Answer, Question
+
+
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+
+    que = pd.read_csv("data/que.csv", sep='|')
+    ans = pd.read_csv("data/ans.csv", sep='|')
+
+    for _, row in que.iterrows():
+        q = Question(id=row['ID'],
+                     text=row['QUESTIOIN'],
+                     right_answer=row['TEACHER_ANSWER'])
+        db.session.add(q)
+    db.session.commit()
+
+    for _, row in ans.iterrows():
+        a = Answer(id=row['ID'],
+                   q_id=row['Q_ID'],
+                   text=row['ANSWER'])
+        db.session.add(a)
+    db.session.commit()
